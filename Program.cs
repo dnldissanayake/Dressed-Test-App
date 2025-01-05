20e6caeaf06f014c134471bb9176eac1ca6fa1ac
dnldissanayake <dnldissanayake1999@gmail.com>
2025-01-05 10:41:20 +0530

    docker inite

--- Program.cs
+++ Program.cs
@@ -34,6 +34,13 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.EnsureCreated();
+}
+
 if (app.Environment.IsDevelopment())
 {
     
